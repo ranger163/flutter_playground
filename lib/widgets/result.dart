a5316f973d48e88ab1fc7e33097828d869cc9d62
ranger163 <ahmed.nassar163@gmail.com>
2020-07-23 13:58:20 +0200

    Displaying total score result

--- lib/widgets/result.dart
+++ lib/widgets/result.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int totalScore;
+
+  Result(this.totalScore);
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Hooray!!\nYou answered all questions.',
+        'Hooray!!\n\nYour is $totalScore.',
         textAlign: TextAlign.center,
       ),
     );
