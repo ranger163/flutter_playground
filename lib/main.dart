fa7593e07cabb46d7b822864a30352639229ba15
ranger163 <ahmed.nassar163@gmail.com>
2020-07-23 13:58:50 +0200

    Adding score and manipulating it

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,34 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
   final _questions = const [
     {
       'questionText': 'What\'s your favourite color?',
-      'answers': ['Green', 'Blue', 'Red', 'Black']
+      'answers': [
+        {'text': 'Green', 'score': 1},
+        {'text': 'Blue', 'score': 12},
+        {'text': 'Red', 'score': 13},
+        {'text': 'Black', 'score': 14}
+      ]
     },
     {
       'questionText': 'What\'s your favourite animal?',
-      'answers': ['Lion', 'Cat', 'Dog', 'Rabbit']
+      'answers': [
+        {'text': 'Lion', 'score': 15},
+        {'text': 'Cat', 'score': 52},
+        {'text': 'Dog', 'score': 25},
+        {'text': 'Rabbit', 'score': 35}
+      ]
     },
     {
       'questionText': 'who\'s your favourite developer?',
-      'answers': ['Nassar', 'Nassar', 'Nassar', 'Nassar']
+      'answers': [
+        {'text': 'Nassar', 'score': 2},
+        {'text': 'Nassar', 'score': 10},
+        {'text': 'Nassar', 'score': 9},
+        {'text': 'Nassar', 'score': 3}
+      ]
     }
   ];
 
@@ -40,12 +56,13 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
